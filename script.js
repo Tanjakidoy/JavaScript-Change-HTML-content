0c2094b992046bad79e249de1182aca707979d84
Tanjakidoy <tanjakidoy@gmail.com>
2024-09-18 22:05:46 +0200

    After Lesson

--- script.js
+++ script.js
@@ -5,12 +5,7 @@ document.getElementById("demo").innerHTML = "Hello JavaScript";
 
 // JavaScript "finds" an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript"
 
-// In HTML, JavaScript code is inserted between <script> and </script> tags
-// Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both
 
-<script>
-document.getElementById("demo").innerHTML = "My First JavaScript";
-</script>
 
 
 
