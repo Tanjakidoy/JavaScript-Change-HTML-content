971ab666f482636d3e60625ffa10dd16baa6bb12
Tanjakidoy <tanjakidoy@gmail.com>
2024-09-18 21:57:27 +0200

    Change HTML Content

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+// JAVASCRIPT CAN CHANGE HTML CONTENT //
+
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+// JavaScript "finds" an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript"
+
+// In HTML, JavaScript code is inserted between <script> and </script> tags
+// Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both
+
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+
+
+
